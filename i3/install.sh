56b6779227cd78daddc7c584f3888cd8bc8a64db
Aryan Choudhary <aryanc403@gmail.com>
2018-08-17 13:12:45 +0530

    Added i3 install.sh

---
+++ i3/install.sh
@@ -0,0 +1,32 @@
+sudo dpkg -i pkg/playerctl-0.6.1_amd64.deb
+sudo apt-get install feh
+cd ~/github/
+git clone git@github.com:aryanc403/WallPapers.git
+#sudo apt-get install arandr
+mkdir ~/.fonts/
+cd dotfiles/i3/pkg/
+cp -r fontawesome-free-5.2.0-desktop/otfs/* ~/.fonts/
+gsettings set org.gnome.desktop.background show-desktop-icons false
+cp -r YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
+sudo apt-get install lxappearance
+#Correct system fonts Part3 7:50
+
+#Install Arc Theme
+sudo apt install autoconf
+sudo apt-get install libgtk-3-dev
+./autogen.sh --prefix=/usr
+sudo make install
+sudo add-apt-repository -u ppa:snwh/ppa
+sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons
+sudo apt install pixmap
+sudo apt-get install gtk2-engines-pixbuf
+sudo apt install gnome-themes-standard
+sudo apt-get install lxappearance gtk-chtheme qt4-qtconfig
+sudo apt install compton
+
+//open lxappearance and select Arc Dark theme and moka icon pack.
+
+cd ~
+
+
+
